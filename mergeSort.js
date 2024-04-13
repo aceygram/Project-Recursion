6c995783e4d678433fe6e52fa06ec1f603ddc8fa
Promise Okeke <ace4facebook@gmail.com>
2024-04-13 10:41:54 +0100

    created the merge algorithm to sort and merge the divide and conquer arrays produced by the merge sort algo

--- mergeSort.js
+++ mergeSort.js
@@ -1,5 +1,24 @@
 //MERGE SORT
+function merge(leftArr, rightArr){
+    const output = [ ];
+    let leftIndex = 0;
+    let rightIndex = 0;
 
+    while(leftIndex < leftArr.length && rightIndex < rightArr.length){
+        const leftEl = leftArr[leftIndex];
+        const rightEl = rightArr[rightIndex];
+
+        if (leftEl <= rightEl){
+            output.push(leftEl)
+            leftIndex++
+        } else {
+            output.push(rightEl)
+            rightIndex++
+        }
+    }
+
+    return [...output, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)]
+}
 
 function mergeSort (array){
     if(array.length <= 1){
